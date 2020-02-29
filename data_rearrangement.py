f7c69a23e6d727c57cd743d1abd22d1f4b449f59
Sharon Komissarov <sharon200102@gmail.com>
2020-02-29 21:48:15 +0200

    data_rearrangment update

--- data_rearrangement.py
+++ data_rearrangement.py
@@ -4,6 +4,3 @@ def minmax_norm(df):
     return (df-df.min())/(df.max()-df.min())
 def zscore_norm(df):
     return (df - df.mean()) / df.std()
-data=pd.read_csv(r'C:\sharon\second_degree\unsupervised_learning\Final Project\Unsupervised_project\creditcard.csv')
-data.drop('Class',inplace=True,axis=1)
-zscore_norm(data).to_csv('MinMax_normalized_data.csv')
