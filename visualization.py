064c5e027efaaf90a31980aaeb782516bfc6d1cf
Sharon Komissarov <sharon200102@gmail.com>
2020-02-29 14:07:26 +0200

    visualization upload

---
+++ visualization.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+# Plots the distribution of all columns by the shape and size inserted as arguments.
+
+def dist_of_cols(col_list,data,figsize,nrows=1,ncols=1,savefig=0,name_of_file=None):
+    idx=0
+    if len(col_list)!=nrows*ncols:
+      raise ValueError("The number of rows an columns inserted doesn't match to the length of col_list argument")
+    fig, axes=plt.subplots(nrows=nrows,ncols=ncols,squeeze=False,figsize=figsize)
+    # run over all lines in the grid.
+    for ax_list in axes:
+    # run over all axes in the line.
+      for ax in ax_list:
+        ax.set_title(col_list[idx]+" disribution")
+        #plot the distribution of the column.
+        sns.distplot(data[col_list[idx]],ax=ax)
+        idx+=1
+    # If the user wants to save the figure.
+    if(savefig==1):
+      fig.savefig(name_of_file)
+    plt.tight_layout() #add spacing to the figure.
+    plt.show()
+#Plots the different distributions of a column inserted based on a categorical feature.
+def dis_by_cat(data,col_name,cat_name,figsize,savefig=0,name_of_file=None):
+  g = sns.FacetGrid(data=data,row=cat_name,height=figsize[0],aspect=figsize[1],sharex=False)
+  g = g.map(sns.distplot, col_name)
+  # If the user wants to save the figure.
+  if(savefig==1):
+      g.savefig(name_of_file)
+  plt.tight_layout()
+  plt.show()
+
+"""
+    sns.barplot(x='Class',y='Amount',data=data )
+    sns.stripplot(x="Class", y="Amount", data=data)
+"""
