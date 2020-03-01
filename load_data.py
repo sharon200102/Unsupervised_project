683143334699dca0098992a5b2def5131a811d4b
zoharyakobi7 <61273544+zoharyakobi7@users.noreply.github.com>
2020-03-01 15:58:02 +0200

    Add files via upload

---
+++ load_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+data = pd.read_csv('creditcard.csv')
+
+
+def minmax_norm(df):
+    return (df-df.min())/(df.max()-df.min())
+
+
+def zscore_norm(df):
+    return (df - df.mean()) / df.std()
+
+
+def kick_anom(dataset, coll_name, limit):
+    for i in range(len(dataset[coll_name])):
+        if dataset.loc[i][coll_name] > limit:
+            dataset = dataset.drop(i)
+    return dataset
+
+
+data = kick_anom(data, "Amount", 5000)
+data = data.drop(columns=["Class"])
+data = zscore_norm(data)
