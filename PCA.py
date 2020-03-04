c5626971dfd8a6bd3e7ec358e7a82f771ac81f38
Sharon Komissarov <sharon200102@gmail.com>
2020-03-04 18:39:02 +0200

    added a decomposition function and edited the docs

--- PCA.py
+++ PCA.py
@@ -1,23 +1,28 @@
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, FastICA
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import load_data as ld
 import seaborn as sns
-def pca_analysis(data,n_components=2,savefig=0,name_of_file=None):
-    pca = PCA(n_components=n_components)
-    pca.fit(data)
-    principalComponents = pca.fit_transform(data)
-    principalDf = pd.DataFrame(data=principalComponents, columns=list(map(lambda num:'principal component '+str(num),range(1,n_components+1))))
+# function for decomposing the data to fewer dimensions using the constructor function inserted.
+def decompose(data,dec_func,n_components=2,savefig=0,name_of_file=None):
+    dec_obj = dec_func(n_components=n_components)
+    dec_obj.fit(data)
+    components = dec_obj.fit_transform(data)
+    #changing the columns names.
+    componentsDF = pd.DataFrame(data=components, columns=list(map(lambda num:'component '+str(num),range(1,n_components+1))))
+    #visualization
     if savefig==1:
         fig = plt.figure()
         if n_components==3:
             ax=Axes3D(fig)
-            ax.scatter(principalDf['principal component 1'],principalDf['principal component 2'],principalDf['principal component 3'])
+            ax.scatter(componentsDF['component 1'],componentsDF['component 2'],componentsDF['component 3'])
             fig.savefig(name_of_file)
         if n_components==2:
             ax=fig.add_axes()
-            sns.scatterplot(principalDf['principal component 1'],principalDf['principal component 2'],data=data,ax=ax)
+            sns.scatterplot(componentsDF['component 1'],componentsDF['component 2'],data=data,ax=ax)
             fig.savefig(name_of_file)
-    return pca,principalDf
+    return dec_obj,componentsDF
+
+decompose(ld.data,FastICA,n_components=2,savefig=1,name_of_file='2d_ica_plot.png')
