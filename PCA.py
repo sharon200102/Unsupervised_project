8072438c7005664d05d43f67a6dbd5512f7664a8
Sharon Komissarov <sharon200102@gmail.com>
2020-03-07 12:14:56 +0200

    Added some graphs

--- PCA.py
+++ PCA.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import load_data as ld
 import seaborn as sns
+from sklearn.feature_selection import mutual_info_regression
 # function for decomposing the data to fewer dimensions using the constructor function inserted.
-def decompose(data,dec_func,n_components=2,savefig=0,name_of_file=None):
+def decompose(data,dec_func,n_components=2,c=None,savefig=0,name_of_file=None):
     dec_obj = dec_func(n_components=n_components)
     dec_obj.fit(data)
     components = dec_obj.fit_transform(data)
@@ -17,12 +18,16 @@ def decompose(data,dec_func,n_components=2,savefig=0,name_of_file=None):
         fig = plt.figure()
         if n_components==3:
             ax=Axes3D(fig)
-            ax.scatter(componentsDF['component 1'],componentsDF['component 2'],componentsDF['component 3'])
+            ax.scatter(componentsDF['component 1'],componentsDF['component 2'],componentsDF['component 3'],c=c)
             fig.savefig(name_of_file)
         if n_components==2:
             ax=fig.add_axes()
-            sns.scatterplot(componentsDF['component 1'],componentsDF['component 2'],data=data,ax=ax)
+            sns.scatterplot(componentsDF['component 1'],componentsDF['component 2'],data=data,hue=c,ax=ax)
             fig.savefig(name_of_file)
     return dec_obj,componentsDF
+def importent_feat(dec_obj,orignal_data_columns):
+    n_components = dec_obj.components_.shape[0]
+    most_important_feat = [np.abs(dec_obj.components_[i]).argmax() for i in range(n_components)]
+    important_feat_names = [orignal_data_columns[index] for index in most_important_feat]
+    return  important_feat_names
 
-decompose(ld.data,FastICA,n_components=2,savefig=1,name_of_file='2d_ica_plot.png')
--- load_data.py
+++ load_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import RobustScaler
+
 data = pd.read_csv('creditcard.csv')
 
 
@@ -10,13 +12,15 @@ def minmax_norm(df):
 def zscore_norm(df):
     return (df - df.mean()) / df.std()
 
+def Robust_std(df):
+    transformer = RobustScaler().fit(df)
+    return pd.DataFrame(transformer.transform(df),columns=df.columns)
 
 def kick_anom(dataset, coll_name, limit):
     for i in range(len(dataset[coll_name])):
         if dataset.loc[i][coll_name] > limit:
             dataset = dataset.drop(i)
     return dataset
-
-
+class_col=data['Class']
 data = data.drop(columns=["Class"])
 data = minmax_norm(data)
